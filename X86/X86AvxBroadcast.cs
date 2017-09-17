32f2da350f1864591f4e9e72b9601dfbe12aac43
Ta Thanh Dinh <tathanhdinh@outlook.com>
2017-09-17 04:34:00 +0200

    starting the project

---
+++ X86/X86AvxBroadcast.cs
@@ -0,0 +1,16 @@
+﻿namespace Gee.External.Capstone.X86 {
+    /// <summary>
+    ///     X86 AVX Broadcast.
+    /// </summary>
+    public enum X86AvxBroadcast {
+        /// <summary>
+        ///     Invalid AVX Broadcast.
+        /// </summary>
+        Invalid = 0,
+
+        Broadcast2,
+        Broadcast4,
+        Broadcast8,
+        Broadcast16
+    }
+}
---
+++ X86/X86AvxRoundingMode.cs
@@ -0,0 +1,16 @@
+﻿namespace Gee.External.Capstone.X86 {
+    /// <summary>
+    ///     X86 AVX Rounding Mode.
+    /// </summary>
+    public enum X86AvxRoundingMode {
+        /// <summary>
+        ///     Invalid AVX Rounding Mode.
+        /// </summary>
+        Invalid = 0,
+
+        RoundNearest,
+        RoundDown,
+        RoundUp,
+        RoundTowardZero
+    }
+}
